3edf744f72638c64ba4d437c385acaa0bf58ee1c
Javi Martín <javim@elretirao.net>
2019-11-07 19:33:45 +0100

    Use an inline CKEditor
    There are several reasons to prefer an inline CKEditor.
    
    First, the user interface is nicer.
    
    Second, when using translations, using CKEditor in a traditional way
    loaded one iframe per translation, causing each iframe to generate its
    own requests to get the CSS CKEditor needs. In development and testing,
    it made loading CKEditor much slower.
    
    Finally, since the inline editor doesn't use iframes, the CSS selectors
    to find an instance are much easier to handle.

--- spec/support/matchers/have_ckeditor.rb
+++ spec/support/matchers/have_ckeditor.rb
@@ -1,27 +1,17 @@
 RSpec::Matchers.define :have_ckeditor do |label, with:|
-  define_method :textarea_id do
-    find("label", text: label)[:for]
-  end
-
-  define_method :ckeditor_id do
-    "#cke_#{textarea_id}"
-  end
-
-  define_method :has_ckeditor? do
-    has_css?("label", text: label) && has_css?(ckeditor_id)
-  end
-
-  match do
-    return false unless has_ckeditor?
-
-    page.within(ckeditor_id) do
-      within_frame(0) { has_content?(with) }
+  define_method :textarea do
+    page.within(".translatable-fields") do
+      has_field?(label, visible: false) && find_field(label, visible: false)
     end
   end
 
+  match do
+    textarea && has_css?("[aria-label~='#{textarea[:id]}']", exact_text: with)
+  end
+
   failure_message do
-    if has_ckeditor?
-      text = page.within(ckeditor_id) { within_frame(0) { page.text } }
+    if textarea
+      text = page.find("[aria-label~='#{textarea[:id]}']").text
 
       "expected to find visible CKEditor '#{label}' with '#{with}', but had '#{text}'"
     else
